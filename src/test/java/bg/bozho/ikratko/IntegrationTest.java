0915cb4229f3c5e46cdceee1816cdd0f94020e6c
Glamdring <glamd@abv.bg>
2017-12-03 15:08:42 +0200

    testing anagrams

--- src/test/java/bg/bozho/ikratko/IntegrationTest.java
+++ src/test/java/bg/bozho/ikratko/IntegrationTest.java
@@ -1,6 +1,6 @@
 package bg.bozho.ikratko;
 
-import junit.framework.Assert;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import bg.bozho.ikratko.other.Anagram;
 import bg.bozho.ikratko.other.RhymeService;
-import bg.bozho.ikratko.Checker;
+import junit.framework.Assert;
 
 @ContextConfiguration(locations="/applicationContext.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,6 +18,9 @@ public class IntegrationTest {
 
     @Autowired
     private RhymeService rService;
+    
+    @Autowired
+    private Anagram anagram;
 	
 	@Autowired
 	private Checker checker;
@@ -27,5 +31,13 @@ public class IntegrationTest {
 		rService.init();
         int rhymes = rService.getRhymes("ова", 3).size();
         Assert.assertTrue("No rhymes found", rhymes > 0);
+    }
+    
+    @Test
+    public void anagramTest() {
+    	checker.initialize();
+    	
+		Set<String> anagrams = anagram.getAnagrams("Барселона");
+        Assert.assertTrue("No anagrams found", anagrams.size() > 0);
     }
 }
